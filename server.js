c2c87a4b91880b7e71ce1ec4e2fbb675ba7028a9
Mira Rajesh <mira1@email.unc.edu>
2023-04-26 10:50:35 -0400

    "a01"

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = args['port'] || process.env.PORT || 3000
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-fs.readFile("./www/index.html", "utf8", (err, data) => {
+fs.readFile("public/index.html", "utf8", (err, data) => {
     if (err) {
       console.error(err)
       return
